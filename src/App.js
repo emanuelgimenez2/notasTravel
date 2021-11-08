b3d3732431714fcc9371133597e21c0695a89a14
emanuel gimenez <emanuelgimenez2@gmail.com>
2021-11-08 20:29:41 -0300

    reparacion errores

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import Contenedor from './components/Contenedor/Contenedor';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 
  function App() {
   
   return (
     <div className="App">
+    <Header/>
     <Contenedor/>
+    <Footer/> 
     </div>
   );
 }
